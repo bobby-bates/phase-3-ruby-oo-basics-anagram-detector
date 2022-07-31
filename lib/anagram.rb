bc00bebbfc72d010ccca9fe0bdd7fc863b0b9882
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-31 09:37:24 -0400

    complete assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize word
+    @word = word
+  end
+
+  def match anagram_arr
+    # Return all matches in arr
+    # If no match, return empty arr
+    anagram_arr.filter { |anagram| word.chars.sort == anagram.chars.sort }
+    # Implicit return of empty arr if no matches
+  end
+end
